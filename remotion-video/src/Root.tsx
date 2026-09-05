57375909cb5996860c6544c17293a2cf623d0ddf
groovyjuice <seiichi.nagasaki@gmail.com>
2026-09-06 00:00:17 +0900

    Add short Remotion smoke-test composition

--- remotion-video/src/Root.tsx
+++ remotion-video/src/Root.tsx
@@ -4,13 +4,23 @@ import {TestVideo} from './Video';
 
 export const Root: React.FC = () => {
   return (
-    <Composition
-      id="TestVideo"
-      component={TestVideo}
-      durationInFrames={900}
-      fps={30}
-      width={1920}
-      height={1080}
-    />
+    <>
+      <Composition
+        id="TestVideo"
+        component={TestVideo}
+        durationInFrames={900}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="SmokeTest"
+        component={TestVideo}
+        durationInFrames={30}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+    </>
   );
 };
